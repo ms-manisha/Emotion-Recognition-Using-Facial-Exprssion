5c32549f5bec565247391c0e6b5f62650ad536b8
ms-manisha <77263225+ms-manisha@users.noreply.github.com>
2021-04-05 17:03:44 +0530

    Add files via upload

---
+++ songs_quotes_list.py
@@ -0,0 +1,60 @@
+songs_dict = {
+"happy": [
+"link 1",
+"link 2"
+],
+"sad": [
+"link 1",
+"link 2"
+],
+"surprise": [
+"link 1",
+"link 2"
+],
+"neutral": [
+"link 1",
+"link 2"
+],
+"disgust": [
+"link 1",
+"link 2"
+],
+"fear": [
+"link 1",
+"link 2"
+],
+"angry": [
+"link 1",
+"link 2"
+]
+}
+quoes_list = {
+"happy": [
+"Quote 1",
+"Quote 2"
+],
+"sad": [
+"Quote 1",
+"Quote 2"
+],
+"surprise": [
+"Quote 1",
+"Quote 2"
+],
+"neutral": [
+"Quote 1",
+"Quote 2"
+],
+"disgust": [
+"Quote 1",
+"Quote 2"
+],
+"fear": [
+"Quote 1",
+"Quote 2"
+],
+"angry": [
+"Quote 1",
+"Quote 2"
+]
+}
